d7abed35774f66385218527aee897a6f0d9297ae
iVerkhov <vrkhv@iCloud.com>
2023-08-08 22:11:25 +0300

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Игорь Верхов on 08.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
